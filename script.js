53e211bc47c71e851531e4acb5211885474d5f73
slepeylukee08 <159123907+slepeylukee08@users.noreply.github.com>
2024-12-15 15:49:59 +0100

    js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Skript pro funkci burger menu na mobilních zařízeních
-const burger = document.getElementById('burger');
-const navLinks = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
